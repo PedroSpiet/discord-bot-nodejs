ad95966b4878122336d359649a6156a6f5b4cc88
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2020-12-28 14:39:57 -0300

    Reaçoes e cargos

--- core.js
+++ core.js
@@ -3,6 +3,8 @@ const config = require('./config.json');
 
 const client = new Discord.Client();
 
+const permissions = config.permissions;
+
 const prefix = config.prefix;
 const commands = require('./scripts/commandsReader')(prefix);
 
@@ -16,7 +18,12 @@ client.on("message", async (msg) => {
         //console.log(await msg.channel.messages.fetch());
         const args = msg.content.split(" ");
         if (commands[args[0]]){
-            commands[args[0]](client, msg)
+            if (verifyPermission(msg.member, args[0])){
+                commands[args[0]](client, msg)
+            }
+            else {
+                msg.reply('Você não tem permissão para executar este comando!');
+            }
         } else {
             msg.reply("Comando não encontrado! digite !commands para ver todos os comandos");
         }
@@ -34,5 +41,26 @@ client.on("guildMemberRemove",(member)=>{
     const welcomeChannel = member.guild.channels.cache.find(channel => channel.id == config.welcomeId);
     welcomeChannel.send(`${member.user} acabou de sair em nosso servidor :(`);
 });
+
+function verifyPermission(member, command) {
+    let verification = !permissions[command];
+    
+    if(!verification) {
+        const perms = permissions[command];
+        perms.forEach(p => {
+            switch(p.type) {
+                case 'role':
+                    if(member.roles.cache.has(p.value)) verification = true;
+                break;
+
+                case 'permission':
+                    if(member.permissions.has(p.value)) verification = true;
+                break;
+            }
+        });
+    }
+    
+    return verification;
+}
 
 client.login(config.token);
